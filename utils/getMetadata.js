0d89924b3aacfe682716cc163b0e050726d93249
Daniel James <djames@alternativeairlines.com>
2023-02-09 17:43:57 +0000

    fix page zoom on mobile

--- utils/getMetadata.js
+++ utils/getMetadata.js
@@ -23,6 +23,7 @@ export function getMetadata(title, type) {
     }
 
     return <> 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         { 
             Object.keys(meta).map(section => {
