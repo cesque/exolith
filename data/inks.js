86e45659cbe62546ccc5e7d9650037248f27bd60
Daniel James <djames@alternativeairlines.com>
2023-12-12 02:03:12 +0000

    update inks

--- data/inks.js
+++ data/inks.js
@@ -2,39 +2,44 @@ export default [
     {
         id: 'pelikan edelstein jade',
         name: 'Pelikan Edelstein Jade',
-        color: 'rgb(27, 174, 135)',
+        color: 'rgb(27 174 135)',
     },
     {
         id: 'pelikan edelstein golden beryl',
         name: 'Pelikan Edelstein Golden Beryl',
-        color: 'rgb(207, 135, 12)',
-        shimmer: 'rgb(255, 204, 0)',
+        color: 'rgb(207 135 12)',
+        shimmer: 'rgb(255 204 0)',
     },
     {
         id: 'diamine enchanted ocean',
         name: 'Diamine Enchanted Ocean',
-        color: 'rgb(31, 78, 108)',
-        shimmer: 'rgb(200, 200, 200)',
+        color: 'rgb(31 78 108)',
+        shimmer: 'rgb(200 200 200)',
     },
     {
         id: 'pilot iroshizuku yama-guri',
         name: 'Pilot Iroshizuku Yama-guri',
-        color: 'rgba(105, 64, 50)',
+        color: 'rgb(105 64 50)',
     },
     {
         id: 'sailor shikiori yamadori',
         name: 'Sailor Shikiori Yamadori',
-        color: 'rgba(8, 55, 77)',
+        color: 'rgb(8 55 77)',
+    },
+    {
+        id: 'sailor shikiori sakuramori',
+        name: 'Sailor Shikiori Sakuramori',
+        color: 'rgb(255 155 194)',
     },
     {
         id: 'diamine oxblood',
         name: 'Diamine Oxblood',
-        color: 'rgb(255, 0, 0)',
+        color: 'rgb(255 0 0)',
     },
     {
         id: 'diamine golden ivy',
         name: 'Diamine Golden Ivy',
-        color: 'rgb(6, 106, 24)',
-        shimmer: 'rgb(255, 204, 0)',
+        color: 'rgb(6 106 24)',
+        shimmer: 'rgb(255 204 0)',
     }
 ]
